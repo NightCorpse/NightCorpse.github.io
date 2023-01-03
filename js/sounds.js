7733b0b4738e21b55640da5df5b4cfcb7898decf
NightCorpse <108156582+NightCorpse@users.noreply.github.com>
2023-01-03 18:08:46 -0300

    Efeitos Sonoros Adicionados

---
+++ js/sounds.js
@@ -0,0 +1,12 @@
+function playAmogus() { 
+    var amogus = document.getElementById("amogus");
+    amogus.volume = 0.2;
+    amogus.currentTime = 0.8;
+    amogus.play(); 
+} 
+
+function playSus() { 
+    var sus = document.getElementById("sus");
+    sus.volume = 0.2;
+    sus.play(); 
+} 
