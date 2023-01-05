2bb7296215f6918e7f2a526f6bca3c7f178d5988
NightCorpse <108156582+NightCorpse@users.noreply.github.com>
2023-01-05 05:17:22 -0300

    Efeitos complementados e reorganizados

--- js/btn_effects.js
+++ js/btn_effects.js
@@ -1,32 +1,39 @@
-let timer, timer2, timer3;
-
-function emergencyCall(){
-    clearTimeout(timer2);
-    clearTimeout(timer3);
-
-    bodyTag = document.getElementsByTagName("BODY")[0];
-    bodyTag.style.filter="invert(100%)";
-    bodyTag.style.transition="filter 1.5s";
-    timer2 = setTimeout(
-        function(){ bodyTag.style.filter="invert(0%)";} , 3000);
-    timer3 = setTimeout(removeEmergencyCallEffect, 4500);
-
-}
+let timer, timer2, timer3, iconTimer;
 
 function sugoma(){
-    clearTimeout(timer)
+    clearTimeout(timer);
+    
+    var icon = document.getElementById('icon');
+    icon.setAttribute('href' , '/assets/img/sugomaIcon.png' );
+    iconTimer = setTimeout(function(){icon.setAttribute('href' , '/assets/img/amogusCyan.png' );}, 2300);
+    
     var sugomaSec = document.getElementById("section1");
-    document.getElementById("separator").style.display="none";
     sugomaSec.style.backgroundImage="url('../assets/img/sugoma.gif?"+Math.random()+"')";
     sugomaSec.style.backgroundSize="cover";
+    document.getElementById("sectionSeparator").style.display="none";
     timer = setTimeout(removeSugomaEffect, 2300);
 
 
 }
 
+function emergencyCall(){
+    clearTimeout(timer2); clearTimeout(timer3); clearTimeout(iconTimer);
+    
+    var icon = document.getElementById('icon');
+    icon.setAttribute('href' , '/assets/img/amogusInverted.png' );
+    iconTimer = setTimeout(function(){icon.setAttribute('href' , '/assets/img/amogusCyan.png' );}, 3000);
+    
+    bodyTag = document.getElementsByTagName("BODY")[0];
+    bodyTag.style.filter="invert(100%)";
+    bodyTag.style.transition="filter 1.5s";
+    
+    timer2 = setTimeout(function(){ bodyTag.style.filter="invert(0%)";} , 3000);
+    timer3 = setTimeout(removeEmergencyCallEffect, 4500);
+}
+
 function removeSugomaEffect(){
     document.getElementById("section1").removeAttribute("style");
-    document.getElementById("separator").removeAttribute("style");
+    document.getElementById("sectionSeparator").removeAttribute("style");
 
 }
 
